a39fa8363790ba0da66d982a98f3046b2a1eed51
Rafsun Jani <1999rafsun@gmail.com>
2025-11-06 20:38:09 +0000

    initial setup

---
+++ assets/js/download.js
@@ -0,0 +1,1 @@
+// JS will be here
---
+++ coa-with-rest-api.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: COA Plugin with REST API
+ * Description: Custom category and fields for WooCommerce products with REST API
+ * Version: 1.0.0
+ * Author: Rafsun Jani
+ * Requires at least: 5.8
+ * Requires PHP: 7.4
+ * WC requires at least: 5.0
+ */
+
+// Prevent direct access
+if (!defined('ABSPATH')) exit;
+
+?>
